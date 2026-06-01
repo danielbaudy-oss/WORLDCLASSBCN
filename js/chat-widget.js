5808a04e1a5f34107d76faf279b7452e2cf5bd38
Daniel Baudy <danielbaudy-oss@users.noreply.github.com>
2026-06-01 11:09:29 +0200

    Sparkle: 50% bigger (42px) and beefier arms

--- js/chat-widget.js
+++ js/chat-widget.js
@@ -336,7 +336,7 @@
   // Create bubble button
   const bubble = document.createElement('button');
   bubble.className = 'chat-bubble';
-  bubble.innerHTML = '<svg width="28" height="28" viewBox="0 0 24 24" fill="white" style="display:block"><path d="M12 1C12 1 13 8 14 10C16 11 23 12 23 12C23 12 16 13 14 14C13 16 12 23 12 23C12 23 11 16 10 14C8 13 1 12 1 12C1 12 8 11 10 10C11 8 12 1 12 1Z"/></svg>';
+  bubble.innerHTML = '<svg width="42" height="42" viewBox="0 0 24 24" fill="white" style="display:block"><path d="M12 1C12 1 13.5 8 15 10C17 11 23 12 23 12C23 12 17 13 15 14C13.5 16 12 23 12 23C12 23 10.5 16 9 14C7 13 1 12 1 12C1 12 7 11 9 10C10.5 8 12 1 12 1Z"/></svg>';
   bubble.title = 'Asistente Profe';
   bubble.onclick = toggleChat;
   document.body.appendChild(bubble);
